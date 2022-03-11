d37aa0b840b9781856a746591e54ea919fd88e47
Drew Griffiths <djamesg@mun.ca>
2022-03-11 19:17:42 -0330

    Update getter/setter methods for Domino class, finished building DeckOfDominos class

--- MinorRelease/Domino.java
+++ MinorRelease/Domino.java
@@ -3,20 +3,86 @@ import java.awt.Color;
 public class Domino
 {
     private Color tile1Color, tile2Color;
-    private int numOfCrowns, numericalVal;
+    private int numOfCrownsTile1, numOfCrownsTile2, numericalVal;
     private int tile1x, tile1y, tile2x, tile2y;
 
 
-    public Domino(Color tile1Color, Color tile2Color, int numOfCrowns, int numericalVal)
+    public Domino(Color tile1Color, Color tile2Color, int numOfCrownsTile1, int numOfCrownsTile2, int numericalVal)
     {
         this.tile1Color = tile1Color;
         this.tile2Color = tile2Color;
-        this.numOfCrowns = numOfCrowns;
+        this.numOfCrownsTile1 = numOfCrownsTile1;
+        this.numOfCrownsTile2 = numOfCrownsTile2;
         this.numericalVal = numericalVal;
         tile1x = 0;
         tile1y = 0;
         tile2x = 0;
         tile2y = 0;
 
+    }
+
+    public Color getTile1Color()
+    {
+        return tile1Color;
+    }
+
+    public Color getTile2Color()
+    {
+        return tile2Color;
+    }
+
+    public int getNumericalVal()
+    {
+        return numericalVal;
+    }
+
+    public int getNumOfCrownsTile1()
+    {
+        return numOfCrownsTile1;
+    }
+
+    public int getNumOfCrownsTile2()
+    {
+        return numOfCrownsTile2;
+    }
+
+    public int getTile1x()
+    {
+        return tile1x;
+    }
+
+    public void setTile1x(int tile1x)
+    {
+        this.tile1x = tile1x;
+    }
+
+    public int getTile1y()
+    {
+        return tile1y;
+    }
+
+    public void setTile1y(int tile1y)
+    {
+        this.tile1y = tile1y;
+    }
+
+    public int getTile2x()
+    {
+        return tile2x;
+    }
+
+    public void setTile2x(int tile2x)
+    {
+        this.tile2x = tile2x;
+    }
+
+    public int getTile2y()
+    {
+        return tile2y;
+    }
+
+    public void setTile2y(int tile2y)
+    {
+        this.tile2y = tile2y;
     }
 }
