1dfe56a844178cffabff4ae46d262b6fe4d1ab8a
Drew Griffiths <djamesg@mun.ca>
2022-03-12 10:30:13 -0330

    Added a starting round method that sets all the tiles, round, dominos remaining and randomly selects a starting player and shows thier gameboard

--- MinorRelease/KingDomino.java
+++ MinorRelease/KingDomino.java
@@ -1,23 +1,28 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class KingDomino 
 {
 
     private FrameManager frameManager;
-	private ArrayList<Domino> deck;
 	private ArrayList<Domino> currentRndDominos;
 	private ArrayList<Domino> nextRndDominos;
 	private DeckOfDominos dealer;
+	private int roundNum, numOfPlayers, currentPlayersTurn;
+	private Random rand;
+
 	
 	public KingDomino()
 	{
-		
+		rand = new Random();
 		currentRndDominos = new ArrayList<>();
 		nextRndDominos = new ArrayList<>();
 		dealer = new DeckOfDominos();
 		frameManager = new FrameManager(this);
         frameManager.showMainFrame();
-		//deck = dealer.createDeck();
+		roundNum = 1;
+		numOfPlayers =2;
+
 	}
 
 	public void startingRound()
@@ -26,5 +31,44 @@ public class KingDomino
 		currentRndDominos = dealer.randomDominos();
 		nextRndDominos = dealer.randomDominos();
 		frameManager.setCurrentRndDominos(currentRndDominos);
+		frameManager.setNextRndDominos(nextRndDominos);
+		frameManager.setRound(roundNum);
+		frameManager.setRemainingDominos(dealer.getRemainingDominos());
+		numOfPlayers = frameManager.getNumOfPlayers();
+		currentPlayersTurn = rand.nextInt(numOfPlayers) + 1;
+		showCurrentPlayerBoard();
+
+	}
+
+	private void showCurrentPlayerBoard()
+	{
+		if (currentPlayersTurn == 1){
+			frameManager.showPlayer1GameBoard();
+		}
+		else if (currentPlayersTurn == 2){
+			frameManager.showPlayer2GameBoard();
+		}
+		else if (currentPlayersTurn ==3){
+			frameManager.showPlayer3GameBoard();
+		}
+		else if (currentPlayersTurn == 4)
+		{
+			frameManager.showPlayer4GameBoard();
+		}
+	}
+
+	public int getRoundNum()
+	{
+		return roundNum;
+	}
+
+	public void incrementRnd()
+	{
+		++roundNum;
+	}
+
+	public int getRemainingDominos()
+	{
+		return dealer.getRemainingDominos();
 	}
 }
