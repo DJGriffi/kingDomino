8f497817ebbc2df4eff435f79dbe1e568465c544
Drew Griffiths <djamesg@mun.ca>
2022-03-18 11:38:34 -0230

    added constructos, variable, getter and setter

--- MinorRelease/Player.java
+++ MinorRelease/Player.java
@@ -1,47 +1,75 @@
 import java.awt.Color;
+import java.util.ArrayList;
 
 public class Player 
 {
 	private String name;
-	private int points;
+	private int points, playerNumber;
 	private boolean AI;
 	private Color colour;
+	private ArrayList<Domino> dominoes;
 	
-	public Player(String name, Color colour, boolean isAI)
+	public Player(String name, Color colour, int playerNumber)
 	{
 		this.name=name;
 		this.colour= colour;
-		this.AI=isAI;
+		this.AI= false;
+		this.playerNumber = playerNumber;
+		points=0;
+		dominoes = new ArrayList<>();
+	}
+
+	public Player(String name, Color colour, int playerNumber, boolean isAI)
+	{
+		this.name=name;
+		this.colour= colour;
+		this.AI= isAI;
+		this.playerNumber = playerNumber;
 		points=0;
 	}
 
-	public String getName() {
+	public String getName() 
+	{
 		return name;
 	}
 
-	public void setName(String name) {
+	public void setName(String name) 
+	{
 		this.name = name;
 	}
 
-	public int getPoints() {
+	public int getPoints() 
+	{
 		return points;
 	}
 
-	public void setPoints(int points) {
+	public void setPoints(int points) 
+	{
 		this.points = points;
 	}
 
-	public boolean isAI() {
+	public boolean isAI() 
+	{
 		return AI;
 	}
 
-	public Color getColour() {
+	public Color getColour() 
+	{
 		return colour;
 	}
 
-	public void setColour(Color colour) {
+	public void setColour(Color colour) 
+	{
 		this.colour = colour;
 	}
+
+	public int getPlayerNumber()
+	{
+		return playerNumber;
+	}
 	
-	
+	public void addDomino(Domino domino)
+	{
+		dominoes.add(domino);
+	}
 }
